faca31ad50c93ed2d84e10962af5a859f60c4090
romil-soni <romil.us@somaiya.edu>
2022-04-20 13:26:31 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
-import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers'
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
-import Token from './artifacts/contracts/Token.sol/Token.json'
+import "./App.css";
+import { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import Token from "./artifacts/contracts/Token.sol/Token.json";
+import {
+  Input,
+  Button,
+  Box,
+  VStack,
+  Heading,
+  useToast,
+} from "@chakra-ui/react";
 
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 function App() {
-  const [greeting, setGreetingValue] = useState()
-  const [userAccount, setUserAccount] = useState()
-  const [amount, setAmount] = useState()
+  const [balance, setBalance] = useState();
+  const [greeting, setGreetingValue] = useState();
+  const [userAccount, setUserAccount] = useState();
+  const [amount, setAmount] = useState();
+  const [fetchedGreeting, setFetchedGreeting] = useState();
+  const toast = useToast();
 
   async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
   async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      console.log({ provider })
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider });
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
       try {
-        const data = await contract.greet()
-        console.log('data: ', data)
+        const data = await contract.greet();
+        setFetchedGreeting(data);
+        console.log("data: ", data);
       } catch (err) {
-        console.log("Error: ", err)
+        console.log("Error: ", err);
       }
-    }    
+    }
   }
 
   async function getBalance() {
-    if (typeof window.ethereum !== 'undefined') {
-      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    if (typeof window.ethereum !== "undefined") {
+      const [account] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
       const balance = await contract.balanceOf(account);
+      setBalance(balance.toString());
       console.log("Balance: ", balance.toString());
     }
   }
 
   async function setGreeting() {
-    if (!greeting) return
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      console.log({ provider })
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
-      await transaction.wait()
-      fetchGreeting()
+      console.log({ provider });
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      await fetchGreeting();
     }
   }
 
   async function sendCoins() {
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
+    if (window.ethereum !== "undefined") {
+      await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
@@ -67,19 +86,48 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+    <VStack spacing={"10"} my="10" mx="32">
+      {fetchedGreeting && (
+        <Box>
+          <Heading>{fetchedGreeting}</Heading>
+        </Box>
+      )}
+      <Button onClick={fetchGreeting}>Fetch Greeting</Button>
+      <Button onClick={setGreeting}>Set Greeting</Button>
+      <Input
+        onChange={(e) => {
+          setGreetingValue(e.target.value);
+          console.log(greeting);
+        }}
+        placeholder="Set greeting"
+      />
+      {balance && (
+        <Box>
+          <Heading>Balance: {balance}</Heading>
+        </Box>
+      )}
 
-        <br />
-        <button onClick={getBalance}>Get Balance</button>
-        <button onClick={sendCoins}>Send Coins</button>
-        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
-        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
-      </header>
-    </div>
+      <Button onClick={getBalance}>Get Balance</Button>
+      <Button
+        onClick={() => {
+          sendCoins().then(
+            toast({
+              status: "success",
+              duration: 5000,
+              title: "Coins sent successfully",
+              description: `Coins successfully sent to the user ${userAccount}`,
+            })
+          );
+        }}
+      >
+        Send Coins
+      </Button>
+      <Input
+        onChange={(e) => setUserAccount(e.target.value)}
+        placeholder="Account ID"
+      />
+      <Input onChange={(e) => setAmount(e.target.value)} placeholder="Amount" />
+    </VStack>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
