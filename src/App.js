10f16d991d2a855686e7a2d4e0fcdb6b244f1792
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-03-27 10:00:28 +0300

    lava sdk providers integration

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
 import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers'
+import { useState, useEffect } from 'react';
+import { ethers, Wallet } from 'ethers'
+import { LavaEthersProvider } from "lava-sdk-providers";
+
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 import Token from './artifacts/contracts/Token.sol/Token.json'
 
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const greeterAddress = "0xE3313d765E5A98f3b0af355d66011D52e4194Ac9"
+const tokenAddress = "0x587FB68Fd721eb8cA7D5A614F6aFdf1BC107D90B"
+const account = "0xc4E057373767a6B89BcB4A347f3Da1511B736464"
+
+require("dotenv").config()
 
 function App() {
   const [greeting, setGreetingValue] = useState()
   const [userAccount, setUserAccount] = useState()
   const [amount, setAmount] = useState()
+  const [provider, setEtherProvider] = useState();
 
-  async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
+   // Init SDK on app start
+   useEffect(() => {
+    const initSDK = async () => {
+      try {
+        const provider = await new LavaEthersProvider({
+          privKey: process.env.REACT_APP_LAVA_CLIENT_KEY,
+          chainID: "GTH1",
+          pairingListConfig: "pairingList.json"
+        });
+        console.log(provider)
+        setEtherProvider(provider);
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    initSDK().catch(console.error);
+  }, []);
 
   async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      console.log({ provider })
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-    }    
+    const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+    try {
+      const data = await contract.greet()
+      console.log('data: ', data)
+    } catch (err) {
+      console.log("Error: ", err)
+    }
   }
 
   async function getBalance() {
-    if (typeof window.ethereum !== 'undefined') {
-      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
-      const balance = await contract.balanceOf(account);
-      console.log("Balance: ", balance.toString());
-    }
+    const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+    const balance = await contract.balanceOf(account);
+    console.log("Balance: ", balance.toString());
   }
 
   async function setGreeting() {
     if (!greeting) return
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      console.log({ provider })
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
-      await transaction.wait()
-      fetchGreeting()
-    }
+
+    const wallet = new Wallet(
+      process.env.REACT_APP_WALLET_KEY,
+    );
+    const signer = wallet.connect(provider);
+    const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+    const transaction = await contract.setGreeting(greeting)
+    await transaction.wait()
+    fetchGreeting()
   }
 
   async function sendCoins() {
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
-      const transaction = await contract.transfer(userAccount, amount);
-      await transaction.wait();
-      console.log(`${amount} Coins successfully sent to ${userAccount}`);
-    }
+    const wallet = new Wallet(
+      process.env.REACT_APP_WALLET_KEY,
+    );
+    const signer = wallet.connect(provider);
+    const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+    const transaction = await contract.transfer(userAccount, amount);
+    await transaction.wait();
+    console.log(`${amount} Coins successfully sent to ${userAccount}`);
   }
 
   return (
